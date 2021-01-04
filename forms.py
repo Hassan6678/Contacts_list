b27f8c06957091278304559bf4219e218aab9649
Hassan <hassanrazacs@hotmail.com>
2021-01-04 21:07:02 +0500

    Add Email classes

--- forms.py
+++ forms.py
@@ -8,3 +8,12 @@ class ContactForm(FlaskForm):
     firstname = StringField('Firstname', validators=[DataRequired(),Length(min=-1, max=50, message='You cannot have more than 50 characters')])
     lastname = StringField('Lastname', validators=[Length(min=-1, max=50, message='You cannot have more than 50 characters')])
     phone = StringField('Phone', validators=[Length(min=-1, max=20, message='You cannot have more than 20 characters')])
+
+
+class EmailForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(min=-1, max=80, message='You cannot have more than 80 characters')])
+
+class AddEmailForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(min=-1, max=80, message='You cannot have more than 80 characters')])
+    contact_id = StringField('Contact_id', validators=[DataRequired(),Length(min=-1, max=50, message='You cannot have more than 50 characters')])
+
